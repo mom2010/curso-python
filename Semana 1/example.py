4397fba218a53c147b088918d7624166dff71afb
Oliva Morillo <oliva.mod@gmail.com>
2024-05-01 10:56:29 +0200

    revision 1

---
+++ Semana 1/example.py
@@ -0,0 +1,1 @@
+C:\Users\oliva\Desktop\PYTHON\visual code\curso-python\Semana 1\example.py
--- prueba.py
+++ prueba.py
@@ -2,3 +2,5 @@ age= input("Cuantos años tienes?")
 name=input("Como te llamas?")
 
 print("Tu nombre es", name, " y tu edad es", age, ".Encantada!")
+
+
